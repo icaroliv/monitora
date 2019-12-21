40197286586c7d7a23eec7f8776589e1b49e6334
icaroliv <icaroliv@hotmail.com>
2019-12-21 18:34:23 -0300

    Updates

---
+++ precache-manifest.597489f5829dcd3c7dc1e3a5676b4535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb714ac96b3d798a27ca440b82793102",
+    "url": "/monitora/index.html"
+  },
+  {
+    "revision": "8f97d3674f60703ac5f0",
+    "url": "/monitora/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "c4d48e869f5f40c1cfdc",
+    "url": "/monitora/static/js/2.98e43f49.chunk.js"
+  },
+  {
+    "revision": "c576a9dd5aa29a8094e2d2efb815e150",
+    "url": "/monitora/static/js/2.98e43f49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f97d3674f60703ac5f0",
+    "url": "/monitora/static/js/main.82e04c7c.chunk.js"
+  },
+  {
+    "revision": "6d6548c4fc05e6bab91e",
+    "url": "/monitora/static/js/runtime-main.ddb717ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monitora/static/media/logo.5d5d9eef.svg"
+  }
+]);
